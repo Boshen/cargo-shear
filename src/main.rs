ee01362e8afd9f3d8d40fd854e8af1ea06d176da
Boshen <boshenc@gmail.com>
2025-04-25 10:53:24 +0800

    Revert "perf: Use mimalloc on pre-built binaries. 1.2x times faster! (#162)"
    fixes #170
    
    This reverts commit da9cac9f97b44712c062682d3e14d2a4f2c309f6.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-#[cfg(feature = "allocator")]
-#[global_allocator]
-static GLOBAL: mimalloc_safe::MiMalloc = mimalloc_safe::MiMalloc;
-
 use std::process::ExitCode;
 
 use cargo_shear::{CargoShear, cargo_shear_options};
